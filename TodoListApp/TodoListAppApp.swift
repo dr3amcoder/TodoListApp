0d9ba080701cda159339f939466c71acd2e84779
dr3amcoder <jannalimpin@hotmail.co.uk>
2023-10-04 12:03:26 +0100

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Janna Limpin on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
